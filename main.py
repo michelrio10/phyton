f5778eda394b9139b0beb2240545380fc7820368
michelrio <36578631+michelrio10@users.noreply.github.com>
2021-12-02 22:13:04 +0000

    calculadora

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-# 🚨 Don't change the code below 👇
-height = input("enter your height (altura ex 1.85) in m: ")
-weight = input("enter your weight (peso) in kg: ")
-# 🚨 Don't change the code above 👆
-
-#Write your code below this line 👇
-
-bmi = ( (float(weight) / float(height) **2) )
-print("Seu BMI é ", int(bmi))
+import calculadora.py
