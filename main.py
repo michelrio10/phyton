eef22f640cb8246163fcaa009a044a4345526111
michelrio <36578631+michelrio10@users.noreply.github.com>
2021-12-02 22:49:35 +0000

    Calcularadora IMC

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-import calculadora.py
+
+# Calcularadora IMC
+# import calculadora.py
+
+from calculadora import Calculadora
+c = Calculadora(1.85,85)
+print('Seu índice de massa corporal (IMC) é', c.bmi(), "kg/m2")
